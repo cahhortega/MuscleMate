d338be2278b5b775174e2f519a2202412051f6b8
Carolina Ortega <cahortega@outlook.com>
2025-12-05 18:01:00 -0300

    Add project

---
+++ MuscleMate/MuscleMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleMateApp.swift
+//  MuscleMate
+//
+//  Created by Carolina Ortega on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MuscleMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
